25b429a2290a39a51510bea1ac2f61b0a868b91d
serhatserter <serhatserter1995@gmail.com>
2022-09-18 00:47:44 +0300

    mechanic ended

--- Assets/Scripts/StartSceneAnimations.cs
+++ Assets/Scripts/StartSceneAnimations.cs
@@ -12,6 +12,9 @@ public class StartSceneAnimations : MonoBehaviour
 
     public TMP_Text Text1, Text2;
     public Button Button;
+
+    public TMP_Text Name;
+    public List<Image> Dialogs;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +30,65 @@ public class StartSceneAnimations : MonoBehaviour
     public void OnStartPlay()
     {
         Button.gameObject.SetActive(false);
+
+        Name.DOFade(0, 0.5f).OnComplete(() =>
+        {
+            Dialogs[0].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+            {
+                Dialogs[0].DOFade(0, 0.5f).SetDelay(3f).OnComplete(() =>
+                {
+                    Dialogs[1].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+                    {
+                        Dialogs[1].DOFade(0, 0.5f).SetDelay(5f).OnComplete(() =>
+                        {
+                            Dialogs[2].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+                            {
+                                Dialogs[2].DOFade(0, 0.5f).SetDelay(5f).OnComplete(() =>
+                                {
+                                    Dialogs[3].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+                                    {
+                                        Dialogs[3].DOFade(0, 0.5f).SetDelay(5f).OnComplete(() =>
+                                        {
+                                            Dialogs[4].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+                                            {
+                                                Dialogs[4].DOFade(0, 0.5f).SetDelay(5f).OnComplete(() =>
+                                                {
+                                                    Dialogs[5].DOFade(1, 0.5f).SetDelay(0.5f).OnComplete(() =>
+                                                    {
+                                                        Dialogs[5].DOFade(0, 0.5f).SetDelay(5f).OnComplete(() =>
+                                                        {
+                                                            OnDarkScreen();
+
+                                                        });
+
+                                                    });
+
+                                                });
+
+                                            });
+
+                                        });
+
+                                    });
+
+                                });
+
+                            });
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+    }
+
+
+    public void OnDarkScreen()
+    {
 
         Panel.DOFade(1, 1f).OnComplete(() =>
         {
